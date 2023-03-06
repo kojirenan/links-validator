afb104436586876e662870aa2d75e8cdb77523d7
kojirenan <kj.renan@icloud.com>
2023-03-06 14:00:35 -0300

    making links extract function

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ function errorApp(error) {
   throw new Error(chalk.red(error.code, "Arquivo não encontrado"))
 };
 
+function linksExtract(text) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+
+  const catchText = [...text.matchAll(regex)];
+
+  const results = catchText.map(catchText => ({
+    [catchText[1]]: catchText[2]
+  }));
+
+  return results;
+}
+
 async function fileCatch(fileDir) {
   try {
     const enconding = "utf-8";
-    const text = await fs.promises.readFile(fileDir, enconding)
-    console.log(chalk.green(text))
+    const text = await fs.promises.readFile(fileDir, enconding);
+
+    console.log(linksExtract(text));
   } catch (error) {
     errorApp(error)
   }
