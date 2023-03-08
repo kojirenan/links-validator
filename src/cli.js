28421bc62944339f61674ae8523ca256940776c0
kojirenan <kj.renan@icloud.com>
2023-03-08 19:07:38 -0300

    create printList for return and errors

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,41 @@
 import chalk from "chalk";
+import fs from "fs";
 import fileCatch from "./index.js";
 
 const path = process.argv;
 
-async function textProcess(path) {
-  const result = await fileCatch(path[2]);
+function printList(result, id = '') {
+  console.log(
+    chalk.yellow("Lista de Links"),
+    chalk.black.bgGreen(id),
+    result
+  );
+};
 
-  console.log(chalk.yellow("lista de links"), (result));
-}
+async function textProcess(argument) {
+  const path = argument[2];
+
+  try {
+    fs.lstatSync(path);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log("Arquivo ou diretório não existente")
+      return
+    }
+  };
+  if (fs.lstatSync(path).isFile()) {
+    const result = await fileCatch(argument[2]);
+
+    printList(result);
+
+  } else if (fs.lstatSync(path).isDirectory()) {
+    const files = await fs.promises.readdir(path);
+
+    files.forEach(async (fileName) => {
+      const list = await fileCatch(`${path}/${fileName}`)
+      printList(list, fileName);
+    });
+  };
+};
 
 textProcess(path);
